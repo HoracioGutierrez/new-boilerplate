6a44ea812077a0928b9f669ef00546f02c838ff4
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-03-05 16:12:25 -0300

    Add dark mode support and install aceternity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(inter.className, "h-[100dvh] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center")}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+          <main className="z-10">
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
 import { auth } from "@/auth";
 import LoginButton from "@/components/LoginButton";
 import LogoutButton from "@/components/LogoutButton";
-import { Button } from "@/components/ui/button";
+import { ThemeToggleButton } from "@/components/ThemeToggleButton";
 
 export default async function HomePage() {
 
   const session = await auth()
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {session ? <LogoutButton /> : <LoginButton />}
-    </main>
+    <>
+      <nav className="flex items-center gap-4">
+        {session ? <LogoutButton /> : <LoginButton />}
+        <ThemeToggleButton />
+      </nav>
+    </>
   );
 }
