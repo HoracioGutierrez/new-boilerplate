469955553a8e49a5dc70f02c4e4809d5517771f9
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-03-15 19:48:40 -0300

    add latest styles and floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/auth";
 import LoginButton from "@/components/LoginButton";
 import LogoutButton from "@/components/LogoutButton";
 import { ThemeToggleButton } from "@/components/ThemeToggleButton";
+import { Button } from "@/components/ui/button";
 
 export default async function HomePage() {
 
@@ -12,6 +13,7 @@ export default async function HomePage() {
       <nav className="flex items-center gap-4">
         {session ? <LogoutButton /> : <LoginButton />}
         <ThemeToggleButton />
+        <Button variant={"secondary"}>test</Button>
       </nav>
     </>
   );
